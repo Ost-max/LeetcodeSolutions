2434f9988a34587b1a916180f969175316001f01
maos1114 <maksim.ostrovskii@NetCracker.com>
2022-12-02 10:08:57 +0300

    Calc - v1

--- src/com/leetcode/BasicCalculator.java
+++ src/com/leetcode/BasicCalculator.java
@@ -9,48 +9,60 @@ import java.util.Stack;
 public class BasicCalculator {
 
     public static void main(String[] args) throws ScriptException {
-        String testExpr = "1 + 1 + (2 + 4 + (2 +3) +  (4 + 3)) +  1 + (2 +2)";
+        //-(1+(4+51 +  2 )-3)+(6+8)
+        // -2+ 1
+        // 1 + 1
+        //1+5-4
+        String testExpr = "1+5-4";
         ScriptEngineManager manager = new ScriptEngineManager();
         ScriptEngine engine = manager.getEngineByName("js");
         Object evalResult = engine.eval(testExpr);
-        int calcResult = new Solution().calculate(testExpr);
+        int calcResult = new Calculator().calculate(testExpr);
         System.out.println("evalResult: " + evalResult + " calcResult: " + calcResult);
         assert (Integer) evalResult == calcResult;
     }
 }
 
 
-class Solution {
+class Calculator {
     private final Stack<Operation> stack = new Stack<>();
+    private int count = 0;
 
     public int calculate(String s) {
-        System.out.println(stack);
         int leftPart = 0;
-        int count = 0;
-        for (char ch : s.toCharArray()) {
+        boolean previousDig = false;
+        while (count < s.length()) {
+            char ch = s.charAt(count);
             count++;
             if (Character.isDigit(ch)) {
-                if (stack.empty()) {
-                    leftPart = Character.getNumericValue(ch);
+                if(previousDig) {
+                    leftPart = new Concat(leftPart).perform(Character.getNumericValue(ch));
                 } else {
-                    int rightPart = Character.getNumericValue(ch);
-                    leftPart = stack.pop().perform(rightPart);
+                    leftPart = Character.getNumericValue(ch);
+                }
+            } else if (ch == '-') {
+                stack.add(new Subtraction(leftPart));
+            }else if (ch == '+') {
+                if(stack.size() > 0) {
+                    leftPart = stack.pop().perform(leftPart);
+                }
+                stack.add(new Addition(leftPart));
+            } else if (ch == '(') {
+                if (stack.empty()) {
+                    leftPart = calculate(s);
+                } else {
+                    leftPart = stack.pop().perform(calculate(s));
                 }
             }
-            if (ch == '-') {
-                stack.add(new Subtraction(leftPart));
+            if((Character.isSpaceChar(ch) || ch == ')') && previousDig && !stack.empty()) {
+                leftPart = stack.pop().perform(leftPart);
             }
-            if (ch == '+') {
-                stack.add(new Addition(leftPart));
-            }
-            if (ch == '(') {
-                return stack.pop().perform(calculate(s.substring(count)));
-            }
+            previousDig = Character.isDigit(ch);
             if (ch == ')') {
                 return leftPart;
             }
         }
-        return leftPart;
+        return stack.isEmpty() ? leftPart :  stack.pop().perform(leftPart);
     }
 }
 
@@ -95,6 +107,26 @@ class Subtraction implements Operation {
     @Override
     public String toString() {
         return "Subtraction{" +
+                "leftPart=" + leftPart +
+                '}';
+    }
+}
+
+class Concat implements Operation {
+    int leftPart;
+
+    public Concat(int leftPart) {
+        this.leftPart = leftPart;
+    }
+
+    @Override
+    public int perform(int rightPart) {
+        return Integer.parseInt(String.valueOf(leftPart) + rightPart);
+    }
+
+    @Override
+    public String toString() {
+        return "Concat{" +
                 "leftPart=" + leftPart +
                 '}';
     }
